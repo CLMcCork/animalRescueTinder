d588f81b95e5a88f5f8d93a42dfd3e01c485a093
CLMcCork <crystallmccorkle@gmail.com>
2021-04-19 20:08:03 -0500

    Added in some js for the form

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use strict"
+
+
+
+let profileForm = document.querySelector('#profileForm')
+profileForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert("Submit!");
+});
