e11d23c959884fc2d22296d846aeafe71d738d89
CLMcCork <crystallmccorkle@gmail.com>
2021-05-04 23:57:29 -0500

    minor edit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 "use strict"
 
-//delete the api key when pushing to github!! until i figure out how to hide files :) 
-const apiKey = '';
-
-
 let profileForm = document.querySelector('#profileForm')
 profileForm.addEventListener('submit', function (e) {
     e.preventDefault();
     alert("Submit!");
 });
+
+
+
+
